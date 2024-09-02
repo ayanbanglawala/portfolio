dbbd1cf8b6ada6153918403804c74c9b69dcea26
MrSky099 <ayanchhipa2278@gmail.com>
2024-09-02 15:16:05 +0530

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,20 @@
 import '../css/home.css';
-import '../index.css';
 const Home = () => {
     return (
-        <div className="bgImg justify-center items-center">
+        <div className="bgImg  h-[86vh] sm:h-[80vh]">
+            <div className='flex flex-col justify-center items-center'>
             <span className='nunito text-[13px]'>DYNAMIC DESIGN OF PORTFOLIO</span>
-            <div className='mainContent flex flex-col justify-center items-center text-[50px] sm:text-[65px] '>
+            <div className='mainContent flex flex-col justify-center items-center text-[45px] sm:text-[60px] '>
                 <span>Welcome to My </span>
                 <span className='purple font-bold'>Portfolio</span>
             </div>
 
-            <span className='mainContent text-[40px] sm:text-[65px] '>My Name is _____</span>
+            <span className='mainContent text-[40px] sm:text-[60px] '>My Name is _____</span>
             
             <div className='footerInfo flex flex-col justify-center items-center text-[20px] sm:text-[25px] '>
-                <span>Hello, I am Ayan Banglawala, a </span>
+                <span>Hello, I am <b>Ayan Banglawala</b>, a </span>
                 <span className='purple font-bold'>MERN stack Developer</span>
+            </div>
             </div>
         </div>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import '../index.css';
 const Nav = () => {
   return (
     <nav>
-        <div className="nav-items bg-red-900">
+        <div className="nav-items text-[12px] px-[20px] py-[8px] lg:px-[40px] lg:text-[14px]">
             <ul>
                 <li>Home</li>
                 <li>Projects</li>
