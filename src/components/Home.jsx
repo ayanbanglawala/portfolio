c172bc60dae404ee4200440305d2ae029315e27b
Ayan <ayanchhipa2278@gmail.com>
2024-09-26 17:18:39 +0530

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,21 +5,24 @@ const Home = () => {
         <>
             <div className="bgImg  h-[86vh] lg:h-[90vh]" id='home'>
                 <div className='flex flex-col justify-center items-center mb-[190px]'>
-                    <span className='nunito text-[13px] pt-10'>DYNAMIC DESIGN OF PORTFOLIO</span>
+                    <span className='nunito text-[13px] pt-10'>PORTFOLIO OF PROJECTS AND SKILLS</span>
                     <div className='mainContent flex flex-col justify-center items-center text-[45px] sm:text-[60px] '>
                         <span>Welcome to My </span>
                         <span className='purple font-bold'>Portfolio</span>
                     </div>
 
-                    <span className='mainContent text-[40px] sm:text-[60px] '>My Name is _____</span>
+                    <span className='mainContent text-[40px] sm:text-[60px] text-center'>I am <span className='purple'>Ayan Banglawala</span></span>
 
-                    <div className='footerInfo flex flex-col justify-center items-center text-[20px] sm:text-[25px] '>
-                        <span>Hello, I am <b>Ayan Banglawala</b>, a </span>
+                    <div className='footerInfo flex flex-col justify-center items-center text-center text-[20px] sm:text-[25px]'>
                         <span className='purple font-bold'>MERN stack Developer</span>
+                        <span className=' text-center'>
+                            dedicated to crafting innovative web applications and <br />delivering exceptional user experiences.
+                        </span>
                     </div>
+
                 </div>
             </div>
-            
+
         </>
     )
 }
