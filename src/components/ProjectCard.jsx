1e8dc62e04870d87cce3517ab4bb3b046de31fa0
Ayan <ayanchhipa2278@gmail.com>
2024-09-27 16:20:32 +0530

    a

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -20,11 +20,11 @@ const ProjectCard = (props) => {
       <div className={`w-full bgImage shadow-[0_0px_15px_rgba(0,0,0,0.15)] hover:shadow-[0_0px_0px_rgba(0,0,0,0)] transition-all duration-300 cursor-pointer ${props.shadowColor} bg-[#04071D] border border-[#1E2034] rounded-lg shadow-lg p-6 flex flex-col justify-center items-center relative overflow-hidden`}>
         <div className="absolute inset-0 bg-cover bg-center opacity-30" style={{ backgroundImage: "url('/path-to-your-image.jpg')" }}></div>
         <h2 className="text-2xl font-bold mb-4 text-white relative z-10">{props.projectName}</h2>
-        
+
         {/* Toggle between the full description and truncated one */}
         <p className="text-gray-400 relative z-10 mb-2">
           {isExpanded ? props.description : truncatedDescription}
-          
+
           {/* Show '...' or 'Read less' based on the state */}
           {props.description.length > charLimit && (
             <span
@@ -36,9 +36,12 @@ const ProjectCard = (props) => {
           )}
         </p>
 
-        <p className="text-gray-300 relative z-10">
-          Technologies Used: <span className="text-purple-400">{props.techUsed}</span>
-        </p>
+        <div  className="flex justify-start w-full mt-4">
+
+          <p className="text-gray-300 relative float-start z-10">
+            Technologies Used: <br /><span className="text-purple-400">{props.techUsed}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
